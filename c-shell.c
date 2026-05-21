645104cad1da96d35a27e1cffac7cc1dba41f5d4
s3lje <emil.seljestokken@outlook.com>
2026-05-21 12:04:27 +0200

    main loop for reading commands

--- c-shell.c
+++ c-shell.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define LINE_BUFFERSIZE 1024
+
 void    shell_loop();
 char*   read_line();
 char**  split_line();
@@ -31,3 +33,36 @@ void shell_loop(){
         free(args);
     } while (status);
 }
+
+char* read_line(){
+    int   bufferSize = LINE_BUFFERSIZE;
+    int   pos        = 0;
+    char* buffer     = malloc(sizeof(char) * bufferSize);
+    int c;
+
+    if (!buffer){
+        fprintf(stderr, "read_line: allocation error...\n");
+        return EXIT_FAILURE;
+    }
+
+    while (1){
+        c = getchar();
+        if (c == EOF || c == '\n'){ // replace end of line with terminator
+            buffer[pos] = '\0';
+            return buffer;
+        } else {
+            buffer[pos] = c;
+        }
+        pos++;
+
+        if (pos >= bufferSize){
+            bufferSize += LINE_BUFFERSIZE;
+            buffer = realloc(buffer, bufferSize);
+            if (!buffer) {
+                fprintf(stderr, "read_line: re-allocation error...\n"); 
+                return EXIT_FAILURE;
+            }
+        }
+    }
+}
+

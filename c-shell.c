af696b1e2d68419f3f6ca651a64ff6e1ccc2dc02
s3lje <emil.seljestokken@outlook.com>
2026-05-21 13:23:36 +0200

    Added the inbuilt functions for cd, help, and exit

--- c-shell.c
+++ c-shell.c
@@ -36,7 +36,43 @@ int (*builtin_func[]) (char**) = {
 int num_builtins(){
     return sizeof(builtin_str) / sizeof(char *);
 }
+/**
+ * Function for changing directory.
+ */
+int chd(char** args){
+    if (args[1] == NULL){
+        fprintf(stderr, "c-shell: expected argument to \"cd\"\n"); 
+    } else {
+        if (chdir(args[1]) != 0){
+            perror("c-shell");
+        }
+    }
+    return 1;
+}
 
+/**
+ * Prints a help menu to the user.
+ */
+int help(char** args){
+    int i;
+    printf("C-SHELL\n");
+    printf("Type a program name followed by its arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for (int i = 0; i < num_builtins(); i++){
+        printf("\t%s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n"); 
+    return 1; 
+}
+
+/**
+ * Quits the shell.
+ */
+int quit(char** args){
+    return EXIT_SUCCESS;
+}
 
 //////////////////////////////////////////////////////////////
 
